b97efb479c69a2c82230513a728e3f6a0d51d66f
Flávio Bianchetti <fjb.developer@gmail.com>
2022-07-19 16:53:26 -0300

    feat:(scraper.py/scrape_novidades) Função scrape_novidades
    Detalhes:
    - Implementada função scrape_novidades.
    
    Requisito 2 atendido.

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -16,7 +17,19 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    result = list()
+    articles = selector.css('div.archive-main > article.entry-preview')
+    for article in articles:
+        result.append(
+            article.css(
+                """div.post-inner >
+                header.entry-header >
+                h2.entry-title >
+                a ::attr(href)"""
+            ).get()
+        )
+    return result
 
 
 # Requisito 3

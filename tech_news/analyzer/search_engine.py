e3716f0bb1a878c21822a9cd9e2fcd25a13d9065
Flávio Bianchetti <fjb.developer@gmail.com>
2022-07-20 23:59:09 -0300

    feat:(search_engine.py/search_by_category) Função search_by_category
    Detalhes:
     - Implementada função search_by_category.
    
    Requisito 9 atendido.

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -54,4 +54,14 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    result = list()
+    search = search_news({
+        'category': {
+            '$regex': f'{category.casefold().capitalize()}',
+        }
+    })
+
+    for item in search:
+        result.append((item['title'], item['url']))
+
+    return result
